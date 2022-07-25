b31eddabd54c6686df297be8e3b06d5bb1b3f6b0
NiharikaAvara <niharika1204@sasi.ac.in>
2022-07-25 12:08:15 +0530

    add

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,18 @@
+class PrimitiveDataTypes
+{
+static byte b;
+static int i;
+static short s;
+static long l;
+static float f;
+static double d;
+public static void main(String[] args)
+{
+System.out.println(b);
+System.out.println(i);
+System.out.println(s);
+System.out.println(l);
+System.out.println(f);
+System.out.println(d);
+}
+}
